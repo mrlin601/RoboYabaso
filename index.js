d585fb0d62d85d61c4ab80f407bd578cd4412e82
zeteticl <cougar.l@hotmail.com>
2018-02-02 19:09:32 +0800

    :D

--- index.js
+++ index.js
@@ -15,14 +15,11 @@ var options = {
 	}
 }
 app.set('port', (process.env.PORT || 5000));
-
 // views is directory for all template files
-
 app.get('/', function(req, res) {
 //	res.send(parseInput(req.query.input));
 	res.send('Hello');
 });
-
 app.post('/', jsonParser, function(req, res) {
 	let event = req.body.events[0];
 	let type = event.type;
@@ -31,19 +28,17 @@ app.post('/', jsonParser, function(req, res) {
 	let rplyToken = event.replyToken;
 	let rplyVal = {};
 	console.log(msg);
-	//如果有訊息,掉到analytics.js 分析需不要 自動回應
-	if (type == 'message' && msgType == 'text') {
+	//如果有訊息, 呼叫handleEvent 分類	
 	try {
-		rplyVal = analytics.parseInput(rplyToken, msg); 
+	rplyVal = handleEvent(event);
 	} 
 	catch(e) {
 		console.log('catch error');
 		console.log('Request error: ' + e.message);
 	}
-	}
 	//把回應的內容,掉到replyMsgToLine.js傳出去
 	if (rplyVal) {
-	replyMsgToLine.replyMsgToLine(rplyToken, rplyVal.text, options, rplyVal.type); 
+	replyMsgToLine.replyMsgToLine(rplyToken, rplyVal, options); 
 	} else {
 	//console.log('Do not trigger'); 
 	}
@@ -57,4 +52,33 @@ app.listen(app.get('port'), function() {
 
 ////////////////////////////////////////
 ///////// 骰組分析放到analytics.js 
-////////////////////////////////////////		
+////////////////////////////////////////	
+
+
+function handleEvent(event) {
+  switch (event.type) {
+    case 'message':
+      const message = event.message;
+      switch (message.type) {
+        case 'text':
+          return analytics.parseInput(event.rplyToken, event.message.text); 
+        default:
+           break;
+      }
+    case 'follow':
+		break;
+    case 'unfollow':
+       break;
+    case 'join':
+break;
+    case 'leave':
+       break;
+    case 'postback':
+       break;
+    case 'beacon':
+      break;
+    default:
+       break;
+  }
+}
+	
